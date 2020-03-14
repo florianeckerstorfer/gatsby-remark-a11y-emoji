e17be140067fe93ec646ba9a08331d6433b00adc
Florian Eckerstorfer <florian@eckerstorfer.net>
2020-03-14 20:39:10 +0100

    test: Fix order of fixtures in integration

--- __tests__/integration.spec.sh
+++ __tests__/integration.spec.sh
@@ -7,51 +7,33 @@ sh -c "rm -rf gatsby-integration-test"
 
 local_registry="http://0.0.0.0:4873"
 
-echo "hello 1"
+# Install Gatsby and
+npx gatsby new gatsby-integration-test https://github.com/gatsbyjs/gatsby-starter-blog
+
+cd gatsby-integration-test
 
 # start local registry
 tmp_registry_log=`mktemp`
-nohup verdaccio --config ./scripts/verdaccio.yaml &>$tmp_registry_log &
-
-echo "hello 2"
+nohup npx verdaccio --config ./scripts/verdaccio.yaml &>$tmp_registry_log &
 
 # wait for `verdaccio` to boot
 grep -q 'http address' <(tail -f $tmp_registry_log)
 
-echo "hello 3"
-
 # login so we can publish packages
 npx npm-auth-to-token -u test -p test -e test@test.com --registry $local_registry
-
-echo "hello 4"
 
 # Run npm command
 npm --registry $local_registry publish
 
-echo "hello 5"
-
-# Install Gatsby and
-npx gatsby new gatsby-integration-test https://github.com/gatsbyjs/gatsby-starter-blog
-
-echo "hello 6"
-
-cd gatsby-integration-test
-
 npm install gatsby-remark-a11y-emoji -r $local_registry 
 
-echo "hello 7"
-
 sed -i -e 's/gatsby-remark-smartypants/gatsby-remark-a11y-emoji/g' gatsby-config.js
-
-echo "hello 8"
 
 # Add Emoji to a Markdown page
 echo '🎸🎤👯‍♂️' >> content/blog/hello-world/index.md
 
 # Build Gatsby
 npx gatsby build
-
-echo "hello 9"
 
 # Check if Emoji was been converted
 if grep -q guitar public/hello-world/index.html; then
