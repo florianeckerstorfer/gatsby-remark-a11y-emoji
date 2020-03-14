54a1138c431f532017cb44a1fcee8a6c7eaa7ae5
Florian Eckerstorfer <florian@eckerstorfer.net>
2020-03-14 10:29:58 +0100

    Update deps

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const emojiRegex = require('emoji-regex');
 const gemoji = require('gemoji');
 const visit = require('unist-util-visit');
 
+function findByEmoji(needle) {
+  return gemoji.find(emoji => emoji.emoji === needle);
+}
+
 function visitor(node) {
   node.value = node.value.replace(emojiRegex(), match => {
     node.type = 'html';
-    const description = gemoji.unicode[match]
-      ? gemoji.unicode[match].description
-      : '';
+    const emoji = findByEmoji(match);
+    const description = emoji ? emoji.description : '';
     return `<span role="img" aria-label="${description}">${match}</span>`;
   });
 }
