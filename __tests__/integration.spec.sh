5ce87d826d586b89274ba7722979028923f06e21
Florian Eckerstorfer <florian.eckerstorfer@mysugr.com>
2020-05-28 17:38:56 +0200

    test: Clean up integration test

--- __tests__/integration.spec.sh
+++ __tests__/integration.spec.sh
@@ -2,51 +2,41 @@
 
 set -e
 
-echo "Clean up previous integration test run..."
+echo ">>> Clean up previous integration test run..."
 sh -c "rm -rf gatsby-integration-test"
 sh -c "rm -rf ./scripts/storage"
 
 local_registry="http://localhost:4873"
 
-original_npm_registry_url=`npm get registry`
-
-echo "Starting Gatsby installation..."
+echo ">>> Starting Gatsby installation..."
 mkdir -p ~/.config/gatsby
 echo '{ "telemetry": { "enabled": false }, "cli": { "packageManager": "npm" } }' > ~/.config/gatsby/config.json
 npx gatsby new gatsby-integration-test https://github.com/gatsbyjs/gatsby-starter-blog
 
-echo "Start local registry..."
+echo ">>> Start local registry..."
 tmp_registry_log=`mktemp`
 echo "Local registry output file=$tmp_registry_log"
 nohup npx verdaccio --config ./scripts/verdaccio.yaml &>$tmp_registry_log &
-
-# Wait for `verdaccio` to boot
 grep -q 'http address' <(tail -f $tmp_registry_log)
 
-echo "Login to local registry and publish package..."
+echo ">>> Login to local registry and publish package..."
 npx npm-auth-to-token -u test -p test -e test@test.com --registry $local_registry
 npm --registry $local_registry publish
 
-echo "Published package"
-
-echo "Install 'gatsby-remark-a11y-emoji' from local registry..."
+echo ">>> Install 'gatsby-remark-a11y-emoji' from local registry..."
 cd gatsby-integration-test
 npm install gatsby-remark-a11y-emoji --registry $local_registry 
 
-npm set registry "$original_npm_registry_url"
-
-echo "Configure plugin and add emoji to a page..."
-sed -i -e 's/gatsby-remark-smartypants/gatsby-remark-a11y-emoji/g' gatsby-config.js
+echo ">>> Configure plugin and add emoji to a page..."
+sed -e 's/gatsby-remark-smartypants/gatsby-remark-a11y-emoji/g' gatsby-config.js
 echo '🎸🎤👯‍♂️' >> content/blog/hello-world/index.md
 
-echo "Build Gatsby..."
+echo ">>> Build Gatsby..."
 npx gatsby build
 
-echo "Check if Emoji was been converted..."
-cat public/hello-world/index.html
+echo ">>> Check if Emoji was been converted..."
+grep -q guitar public/hello-world/index.html
+contains_a11y=$?
+echo "Test complete: contains_a11y=$contains_a11y"
 
-if grep -q guitar public/hello-world/index.html; then
-  exit 0
-else
-  exit 1
-fi
+exit $contains_a11y
